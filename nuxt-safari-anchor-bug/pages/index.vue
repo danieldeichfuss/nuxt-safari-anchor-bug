69972950702f12c9bd1db6acf317df4ba3950118
danieldeichfuss <d.deichfuss@gmail.com>
2021-07-18 21:02:14 +0200

    Reproduce issue

---
+++ nuxt-safari-anchor-bug/pages/index.vue
@@ -0,0 +1,110 @@
+<template>
+<div>
+  <p>
+    Ipsum montes natoque ipsum mattis nostra aenean orci morbi laoreet. Mattis
+    convallis dignissim enim nec congue venenatis malesuada laoreet felis?
+    Ultricies vulputate potenti dapibus nec cursus quis cras. Erat bibendum
+    tristique hendrerit vel rutrum per vivamus dis. Dictum gravida bibendum
+    penatibus auctor luctus. Conubia mi vehicula euismod a, fames metus varius
+    sollicitudin feugiat lacus tortor. Faucibus pharetra at parturient molestie
+    mollis iaculis egestas aenean bibendum viverra mattis fermentum. Mollis
+    platea egestas lorem. Elit orci tempor pulvinar tempus nisl nec morbi. Et
+    cum neque potenti scelerisque ante venenatis orci donec. Metus feugiat
+    inceptos.
+  </p>
+  <p>
+    Feugiat vel nullam dignissim vehicula ridiculus fusce. Pretium aptent risus
+    arcu eros potenti curae; ultrices. Porta netus felis id varius tempus
+    pharetra congue dignissim dis. Ipsum lectus porta nec. Diam justo ac
+    facilisi at elementum taciti sem nam cubilia nam litora. Ullamcorper
+    torquent libero class nostra! Metus eleifend rhoncus primis.
+  </p>
+  <p>
+    Scelerisque vestibulum sem ultricies netus consectetur potenti placerat
+    gravida imperdiet praesent quisque. Laoreet, sit porttitor velit fames
+    tempor imperdiet elementum. Metus purus donec posuere duis nisi ipsum!
+    Feugiat diam adipiscing senectus nascetur. Nisl ligula ridiculus nascetur
+    conubia velit. Lacinia magna condimentum dis nisi lacus tristique eleifend.
+    Netus integer, sodales at?
+  </p>
+  <p>
+    Nibh rhoncus venenatis lacinia feugiat quis nisl. Pretium, primis in
+    inceptos. Odio turpis, eu fringilla hendrerit lectus dictum nisl? Mattis
+    nullam vel convallis arcu sagittis pulvinar justo dictumst diam placerat
+    cum. Diam netus odio eget diam potenti ligula ante mollis feugiat! Commodo
+    pretium faucibus, praesent habitasse luctus interdum faucibus enim sem
+    dignissim erat. Nostra cubilia facilisis in potenti accumsan quis
+    suspendisse per vel fusce. Senectus turpis feugiat varius vulputate egestas
+    sed accumsan dictumst faucibus fames adipiscing accumsan. Litora taciti cras
+    elementum himenaeos enim donec.
+  </p>
+  <p>
+    Non montes ligula per quis netus ut vitae hac, ut neque. Magna curae; class
+    morbi ullamcorper varius facilisi cubilia netus ullamcorper sit cubilia.
+    Ultricies varius massa primis duis penatibus felis condimentum mauris id,
+    dapibus erat. Condimentum scelerisque quam hendrerit. Tincidunt natoque
+    interdum lacus per molestie pharetra morbi vulputate mollis cras litora
+    phasellus. Ridiculus class parturient amet venenatis ridiculus venenatis
+    proin diam. Ornare facilisi ad netus vitae. Per cubilia lorem eu inceptos
+    sollicitudin conubia malesuada. Mattis auctor praesent habitant arcu
+    venenatis libero maecenas accumsan. Venenatis libero vitae proin maecenas
+    magna sem.
+  </p>
+  <p>
+    Suspendisse sapien turpis urna primis sit magna sollicitudin donec ipsum
+    lobortis purus fames. Laoreet lorem class faucibus aliquet eleifend, in
+    aptent urna. Platea tincidunt quam cum ante parturient. Tempus class nam,
+    facilisis id. Aliquet phasellus lobortis dis dolor! Nunc consectetur
+    vehicula habitasse cum hac class nascetur amet tortor nisl. Pretium platea
+    fames in curae; aenean amet aliquet erat parturient sapien. Bibendum sem
+    purus cras tempor fusce dui porttitor magnis. Lobortis consequat diam
+    egestas aptent consequat consectetur nulla parturient non leo fames aenean.
+    Nascetur.
+  </p>
+  <p>
+    Sed, integer taciti nec. Praesent magnis parturient cubilia blandit a. Mi
+    torquent lacus nostra. Mollis urna fermentum nullam. Dolor aliquet dui,
+    pretium velit. Fames varius augue, quisque molestie dictumst dapibus.
+    Lacinia nascetur montes hac elementum feugiat etiam sed class ultricies
+    quam? A vulputate congue mattis lacus. Nostra justo ad elementum felis vel.
+    Placerat lorem pretium senectus congue egestas faucibus natoque elit vivamus
+    sit. Enim taciti tincidunt conubia magna tellus adipiscing eros amet fusce
+    velit faucibus aliquam? Id mollis cum sit aptent dapibus.
+  </p>
+  <p>
+    Suscipit proin euismod, magna taciti per. Ad habitant hac imperdiet dolor
+    augue consequat sociis volutpat. Sed dui dui suscipit. Aenean sapien dictum,
+    hendrerit etiam feugiat metus hac dignissim iaculis vitae nascetur? Lectus
+    risus malesuada dolor hac mattis interdum mollis venenatis dolor. Malesuada
+    vestibulum ridiculus cum eget aptent ligula in hendrerit habitasse curae;
+    lobortis. Lorem blandit tempor ullamcorper mus magna luctus hendrerit fames
+    tortor leo at nullam. Aliquet.
+  </p>
+  <p>
+    Tempor tempor aliquet cum suspendisse nunc senectus pharetra parturient eu
+    metus ac suscipit. Eros class aenean primis ac ipsum? Risus sodales litora
+    suscipit litora dis turpis orci semper. Nisl aptent nibh interdum, ornare
+    ac. Habitasse sem fringilla tempor risus purus fermentum nulla sodales.
+    Habitant maecenas commodo a. Justo sagittis nisl elit conubia habitasse
+    justo volutpat phasellus, volutpat sit. Pellentesque elit nascetur tempor
+    nisl semper varius. Pulvinar lacinia elementum sociosqu montes litora netus.
+    Tristique ut cursus interdum; sodales accumsan molestie nullam tellus? Enim
+    velit.
+  </p>
+  <h1 id="test-anchor">Test Anchor</h1>
+  <p>
+    Hac placerat congue venenatis vulputate accumsan phasellus parturient
+    penatibus torquent sociosqu pulvinar. Donec cum quam convallis rutrum
+    fringilla. Nascetur phasellus primis eleifend nunc euismod vitae, mi
+    senectus montes facilisi habitant feugiat. Ut iaculis lorem justo placerat
+    auctor. Nibh integer sem proin ultrices. Etiam facilisi cum placerat quis,
+    vehicula lacus nullam vestibulum vehicula per commodo. Imperdiet arcu
+    iaculis fames eros est nascetur per commodo. Netus habitasse, duis arcu duis
+    odio litora cursus cras lorem.
+  </p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
